3c124112fb7ac617466c9a201f85cd2871f8e32f
Fave-Dolapo <favourolugbenga27@gmail.com>
2024-01-02 22:07:28 +0100

    first commit

---
+++ chessboard.js
@@ -0,0 +1,12 @@
+let size = 8;
+let chessboard = " ";
+for (let i = 0; i < size; i++){
+    for (let j = 0; j < size; j++){
+        chessboard += (i + j)% 2 === 0? " ":'#'
+    }
+    chessboard += '\n';
+}
+//'i' is for the horizontal rows and 'j' is for vertical coloumns.//
+// '===' is used cause it's a strict equality sign than '=='
+console.log(chessboard)
+
